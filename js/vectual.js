5fc048a11d4ae974798b5de545ef8c3fbe92ee1a
adius <adriansieber@web.de>
2012-02-24 14:35:53 +0100

    fixed pie chart

--- js/vectual.js
+++ js/vectual.js
@@ -1,70 +1,65 @@
 (function (window, document, undefined) {
-    var xhtmlNS,
-        svgNS,
-        xlinkNS;
-
-    xhtmlNS = "http://www.w3.org/1999/xhtml";
-    svgNS = "http://www.w3.org/2000/svg";
-    xlinkNS = "http://www.w3.org/1999/xlink";
-
-    var svg;
+    var svg,
+        c,
+        xhtmlNS = "http://www.w3.org/1999/xhtml",
+        svgNS = "http://www.w3.org/2000/svg",
+        xlinkNS = "http://www.w3.org/1999/xlink";
 
     function attr(obj, attributes) {
         for (var prop in attributes) {
-            obj.setAttributeNS(null, prop, attributes[prop]);
+            if (attributes.hasOwnProperty(prop)){
+                obj.setAttribute(prop, attributes[prop]);
+            }
         }
     }
 
-    function v(c) {
+    function toRad(degrees) {
+        return degrees * (Math.PI / 180);
+    }
 
-        var defs,
-            bg,
-            title,
-            pie,
-            radius,
-            startx,
-            starty,
-            angle,
-            lastx,
-            lasty,
-            angle_all,
-            angle_all_last,
-            graphHeight,
-            graphWidth,
-            width,
-            height,
-            circle,
-            totalValue,
-            maxValue,
-            sortedKeys,
-            numKeys,
-            color,
-            position,
-            angle_this,
-            angle_rad,
-            angle_translate,
-            angle_add,
-            angle_translate_deg,
-            tx,
-            ty,
-            nextx,
-            nexty,
-            xValues,
-            yValues,
-            sector,
-            sortedValues,
-            size,
-            at,
-            atra,
-            path,
-            ani,
-            aniTransform,
-            text,
-            animate;
+    function v(config) {
 
-        c.colors = ['yellow', 'green', 'blue', 'brown'];
+        var defs, bg, title, size, text;
 
-        function Element(type) {
+        c = config;
+
+        c.colors = ['yellow', 'green', 'blue', 'brown', 'grey', 'yellow', 'green', 'blue', 'brown', 'yellow', 'green', 'blue', 'brown'];
+        c.tuples = [];
+        c.sortedValues = [];
+        c.sortedKeys = [];
+        c.size = 0;
+        c.totalValue = 0;
+        c.max = {};
+
+        for (var i in c.data) {
+
+            if (c.data.hasOwnProperty(i)) {
+
+                if (c.data[i] > c.max.val) {
+                    c.max.key = i;
+                    c.max.val = c.data[i];
+                }
+
+                c.totalValue += Number(c.data[i]); //get sum of all Values
+
+                c.size++; //get length of object
+
+                c.tuples.push([i, c.data[i]]); //get sortable array
+            }
+        }
+
+        //sort array
+        c.tuples.sort(function (a, b) {
+            return a[1] < b[1] ? 1 : a[1] > b[1] ? -1 : 0
+        });
+
+        //split into key/value arrays
+        for (var a = 0; a < c.tuples.length; a++) {
+            c.sortedKeys.push(c.tuples[a][0]);
+            c.sortedValues.push(c.tuples[a][1]);
+        }
+
+        function el(type) {
             return document.createElementNS(svgNS, type)
         }
 
@@ -75,7 +70,7 @@
             'class':(c.inline) ? 'vectual_inline' : 'vectual',
             'width':c.width,
             'height':c.height,
-            'viewBox':"0 0 400 200"
+            'viewBox':"0 0 " + c.width + " " + c.height
         });
         document.getElementById(obj.id).appendChild(svg);
 
@@ -100,40 +95,25 @@
             'class':'vectual_title',
             'x':20,
             'y':(10 + c.height * 0.05),
-            'style':'font-size:' + (obj.height * 0.05) + 'px'
+            'style':'font-size:' + (c.height * 0.05) + 'px'
         });
         title.appendChild(document.createTextNode(c.title));
         svg.appendChild(title);
 
-        var funcs = {
-            'pieChart': new Pie(c),
+        return {
+            'pieChart':new Pie(c),
             'test':'test'
         };
-
-        return funcs;
     }
 
-    function Pie(c) {
+    function Pie() {
         var pie,
-            startx,
-            starty,
-            angle,
-            lastx,
-            lasty,
-            angle_all,
-            angle_all_last,
-            graphHeight,
-            graphWidth,
-            width,
-            height,
             circle,
-            totalValue,
-            maxValue,
-            sortedKeys,
-            numKeys,
-            color,
             text,
-            radius = Math.min(c.height, c.width * 0.2);
+            radius = Math.min(c.height, c.width * 0.2),
+            lastx = -radius,
+            lasty = 0,
+            angle_all = 0;
 
         pie = document.createElementNS(svgNS, 'g');
         pie.setAttribute('transform', 'translate(' + (0.5 * c.width) + ', ' + (0.5 * c.height) + ')');
@@ -141,7 +121,7 @@
         svg.appendChild(pie);
 
 
-        if (totalValue == maxValue) { //only one value
+        if (c.totalValue == c.max.val) { //only one value
 
             circle = document.createElementNS(svgNS, 'circle');
             attr(circle, {
@@ -154,109 +134,69 @@
 
             pie.appendChild(circle);
 
-            text = document.createElement('text');
-            text.setAttribute('class', 'vectual_pie_text_single, vectual_pie_text');
-            text.setAttribute('x', '0');
-            text.setAttribute('y', '0');
-            text.setAttribute('style', 'font-size:' + (radius * 0.3) + 'px');
-            text.setAttribute('text-anchor', 'middle');
-            text.setAttribute('stroke-width', (radius * 0.006));
-
-
+            text = document.createElementNS(svgNS, 'text');
+            attr(text, {
+                'class':'vectual_pie_text_single, vectual_pie_text',
+                'x':'0',
+                'y':'0',
+                'style':'font-size:' + (radius * 0.3) + 'px',
+                'text-anchor':'middle',
+                'stroke-width':(radius * 0.006)
+            });
             text.appendChild(document.createTextNode('test'));
             pie.appendChild(text);
 
-        } else {
-            for (i = 0; i < numKeys; i++) { //Pie sectors
+        }
+
+        else {
+            for (var i = 0; i < c.size; i++) { //Pie sectors
                 drawSector(i);
             }
         }
 
         function drawSector(i) {
+            var position, angle_all_rad, angle_all_last, angle_this, angle_translate, angle_add,
+                trans_deg, tx, ty, sector, size, at, atra, path, ani,
+                aniTransform, text, animate, title, nextx, nexty;
 
-            var radius,
-                position,
-                angle_all_last,
-                angle_all,
-                angle_this,
-                angle_rad,
-                angle_translate,
-                angle_add,
-                angle_translate_deg,
-                tx,
-                ty,
-                nextx,
-                nexty,
-                xValues,
-                yValues,
-                sector,
-                sortedValues,
-                totalValue,
-                size,
-                at,
-                atra,
-                path,
-                ani,
-                aniTransform,
-                text,
-                animate,
-                lastx,
-                lasty,
-                title;
-
-            startx = (0.5 * width);
-            starty = (0.5 * height);
-            angle = 0;
-            lastx = -c.radius;
-            lasty = 0;
-
-            angle_all_last = angle_all;
-
-            if (((sortedValues[i] / totalValue) * 360) > 180) size = '0 1,0';
+            if (((c.sortedValues[i] / c.totalValue) * 360) > 180) size = '0 1,0';
             else size = '0 0,0';
 
             //Angle
             angle_all_last = angle_all;
-            angle_this = ((sortedValues[i] / totalValue) * 360);
+            angle_this = ((c.sortedValues[i] / c.totalValue) * 360);
             angle_all = angle_this + angle_all;
-            angle_rad = deg2rad(angle_all);
+            angle_all_rad = toRad(angle_all);
 
-            //Shift direction: add previous angle to half of the current
+            //Shift direction of sector: add previous angle to half of the current
             angle_add = angle_this / 2;
-            angle_translate_deg = angle_all_last + angle_add;
-            angle_translate = deg2rad(angle_translate_deg);
+            trans_deg = angle_all_last + angle_add;
+            angle_translate = toRad(trans_deg);
 
-            tx = -(cos(angle_translate)) * radius;
-            ty = (sin(angle_translate)) * radius;
+            tx = -(Math.cos(angle_translate)) * radius;
+            ty = (Math.sin(angle_translate)) * radius;
 
-            nextx = -(cos(angle_rad) * radius);
-            nexty = (sin(angle_rad) * radius);
+            nextx = -(Math.cos(angle_all_rad) * radius);
+            nexty = (Math.sin(angle_all_rad) * radius);
 
-            if (angle_translate_deg > 0 && angle_translate_deg <= 75) position = 'end';
-            if (angle_translate_deg > 75 && angle_translate_deg <= 105) position = 'middle';
-            if (angle_translate_deg > 105 && angle_translate_deg <= 255) position = 'start';
-            if (angle_translate_deg > 255 && angle_translate_deg <= 285) position = 'middle';
-            if (angle_translate_deg > 285 && angle_translate_deg <= 360) position = 'end';
-
-            xValues = -(cos(angle_translate) * radius);
-            yValues = (sin(angle_translate) * radius);
+            position = (trans_deg <= 75) ? 'end' : (trans_deg <= 105) ? 'middle' :
+                (trans_deg <= 255) ? 'start' : (trans_deg <= 285) ? 'middle' : 'end';
 
 
-            sector = pie.addChild('g');
+            sector = pie.appendChild(document.createElementNS(svgNS, 'g'));
             sector.setAttribute('class', 'vectual_pie_sector');
 
-            if (animations) {
-                at = sector.addChild('animateTransform');
+            if (c.animations) {
+                at = sector.appendChild(document.createElementNS(svgNS, 'animateTransform'));
                 at.setAttribute('attributeName', 'transform');
                 at.setAttribute('begin', 'mouseover');
                 at.setAttribute('type', 'translate');
                 at.setAttribute('to', (tx * 0.2) + ', ' + (ty * 0.2));
-
                 at.setAttribute('dur', '0.3s');
                 at.setAttribute('additive', 'replace');
                 at.setAttribute('fill', 'freeze');
 
-                atra = sector.addChild('animateTransform');
+                atra = sector.appendChild(document.createElementNS(svgNS, 'animateTransform'));
                 atra.setAttribute('attributeName', 'transform');
                 atra.setAttribute('begin', 'mouseout');
                 atra.setAttribute('type', 'translate');
@@ -266,65 +206,67 @@
                 atra.setAttribute('fill', 'freeze');
             }
 
-            path = sector.addChild('path');
+            path = sector.appendChild(document.createElementNS(svgNS, 'path'));
             path.setAttribute('class', 'vectual_pie_sector_path');
-            path.setAttribute('style', 'stroke-width:' + (radius * 0.015) + ';fill:' + color[i]);
+            path.setAttribute('style', 'stroke-width:' + (radius * 0.015) + ';fill:' + c.colors[i]);
             path.setAttribute('d', 'M 0,0 L ' + lastx + ',' + lasty + ' A ' + radius + ',' + radius + ' ' + size + ' ' + nextx + ',' + nexty + ' z');
             path.setAttribute('transform', '');
 
 
-            if (animations) {
-                ani = path.addChild('animate');
+            if (c.animations) {
+                ani = path.appendChild(document.createElementNS(svgNS, 'animate'));
                 ani.setAttribute('attributeName', 'opacity');
                 ani.setAttribute('from', '0');
                 ani.setAttribute('to', '1');
                 ani.setAttribute('dur', '0.6s');
                 ani.setAttribute('fill', 'freeze');
 
-                aniTransform = path.addChild('animateTransform');
+                aniTransform = path.appendChild(document.createElementNS(svgNS, 'animateTransform'));
                 aniTransform.setAttribute('attributeName', 'transform');
                 aniTransform.setAttribute('type', 'rotate');
-                aniTransform.setAttribute('dur', '0.8s');
+                aniTransform.setAttribute('dur', '1s');
                 //aniTransform.setAttribute('calcMode','spline');
                 //aniTransform.setAttribute('keySplines','0 0 0 1');
                 aniTransform.setAttribute('values', angle_all_last + ',0,0; 0,0,0');
                 aniTransform.setAttribute('additive', 'replace');
                 aniTransform.setAttribute('fill', 'freeze');
 
-
-                fanOut = path.addChild('animate');
-                fanOut.setAttribute('attributeName', 'd');
-                //fanOut.setAttribute('from','M 0,0 L '+lastx+','+lasty+' A '+radius+','+radius+' '+size+' '+lastx+','+lasty+' z');
-                //fanOut.setAttribute('to','M 0,0 L '+lastx+','+lasty+' A '+radius+','+radius+' '+size+' '+nextx+','+nexty+' z');
-                fanOut.setAttribute('values', 'M 0,0 L ' + lastx + ',' + lasty + ' A ' + radius + ',' + radius + ' ' + size + ' ' + lastx + ',' + lasty + ' z; ' +
-                    'M 0,0 L ' + lastx + ',' + lasty + ' A ' + radius + ',' + radius + ' ' + size + ' ' + xValues + ',' + yValues + ' z; ' +
-                    'M 0,0 L ' + lastx + ',' + lasty + ' A ' + radius + ',' + radius + ' ' + size + ' ' + nextx + ',' + nexty + ' z; ');
-                fanOut.setAttribute('dur', '0.8s');
+                /*
+                 fanOut = path.addChild('animate');
+                 fanOut.setAttribute('attributeName', 'd');
+                 //fanOut.setAttribute('from','M 0,0 L '+lastx+','+lasty+' A '+radius+','+radius+' '+size+' '+lastx+','+lasty+' z');
+                 //fanOut.setAttribute('to','M 0,0 L '+lastx+','+lasty+' A '+radius+','+radius+' '+size+' '+nextx+','+nexty+' z');
+                 fanOut.setAttribute('values', 'M 0,0 L ' + lastx + ',' + lasty + ' A ' + radius + ',' + radius + ' ' + size + ' ' + lastx + ',' + lasty + ' z; ' +
+                 'M 0,0 L ' + lastx + ',' + lasty + ' A ' + radius + ',' + radius + ' ' + size + ' ' + xValues + ',' + yValues + ' z; ' +
+                 'M 0,0 L ' + lastx + ',' + lasty + ' A ' + radius + ',' + radius + ' ' + size + ' ' + nextx + ',' + nexty + ' z; ');
+                 fanOut.setAttribute('dur', '0.8s');
+                 */
 
             }
 
 
-            text = sector.addChild('text', sortedKeys[i]);
+            text = sector.appendChild(document.createElementNS(svgNS, 'text'));
+            text.appendChild(document.createTextNode(c.sortedKeys[i]));
             text.setAttribute('class', 'vectual_pie_text');
             text.setAttribute('x', (tx * 1.2));
             text.setAttribute('y', (ty * 1.2));
             text.setAttribute('text-anchor', position);
             text.setAttribute('style', 'font-size:' + (angle_this * radius * 0.002 + 8) + 'px');
-            text.setAttribute('fill', color[i]);
+            text.setAttribute('fill', c.colors[i]);
             text.setAttribute('transform', 'translate(0, 5)');
 
 
-            if (animations) {
-                animate = text.addChild('animate');
+            if (c.animations) {
+                animate = text.appendChild(document.createElementNS(svgNS, 'animate'));
                 animate.setAttribute('attributeName', 'opacity');
                 animate.setAttribute('begin', '0s');
                 animate.setAttribute('values', '0;0;1');
-                animate.setAttribute('dur', '0.9s');
-                animate.setAttribute('additive', 'replace');
+                animate.setAttribute('dur', '1s');
                 animate.setAttribute('fill', 'freeze');
             }
 
-            title = sector.addChild('title', sortedKeys[i] + ' | ' + sortedValues[i] + ' | ' + (round(sortedValues[i] / totalValue * 100, 2) ) + '%');
+            title = sector.appendChild(document.createElementNS(svgNS, 'title'));
+            title.appendChild(document.createTextNode(c.sortedKeys[i] + ' | ' + c.sortedValues[i] + ' | ' + (Math.round(c.sortedValues[i] / c.totalValue * 100, 2) ) + '%'));
 
             lastx = nextx;
             lasty = nexty;
@@ -503,8 +445,6 @@
      }
      */
 
-
     vectual = v;
 
-})
-    (window, document);
+})(window, document);
