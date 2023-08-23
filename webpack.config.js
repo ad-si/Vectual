ef19ab2291f9197d6f18dd28c0c8b38613e90eab
Loïc Knuchel <loic.knuchel@doctolib.com>
2023-08-23 17:30:17 +0200

    Upgrade JS dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
+/* eslint-disable-next-line @typescript-eslint/no-var-requires */
 const path = require('path')
 
 module.exports = {
+  mode: 'production',
   entry: './src/javascript/index.js',
   devtool: 'inline-source-map',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
   },
-  mode: 'production',
 }
